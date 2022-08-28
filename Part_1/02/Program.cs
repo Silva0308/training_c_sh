d79c734525af05c09ea3a8c82eee8f3a5fc1494d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-28 21:00:17 +0300

    comparsion

---
+++ Part_1/02/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Please write the first number");
+int a =int.Parse(Console.ReadLine());
+Console.WriteLine("Please write the second number");
+int b =int.Parse(Console.ReadLine());
+if (a==b)
+{
+    Console.WriteLine("This numbres are equal");
+}
+else if (a>b)
+{
+    Console.WriteLine($"{a}>{b}");
+}
+else
+{
+    Console.WriteLine($"{a}<{b}");
+}
