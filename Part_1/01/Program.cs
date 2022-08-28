c8afddf0ba7c8352525328eee5f1aac78342e608
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-28 20:50:15 +0300

    is this a square

---
+++ Part_1/01/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Please, put the first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, put the second number");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine ("It is the square");
+}
+else
+{
+    Console.WriteLine ("It is not the square");
+}
