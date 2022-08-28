ef9f1756e7cf00ba4c72374d10d6030b85c01b8e
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-28 21:21:02 +0300

    max number

---
+++ Part_1/04/Program.cs
@@ -0,0 +1,23 @@
+﻿// 4. Найти максимальное из трех чисел
+int MaxNumber(int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+        if (c > max)
+        {
+            max = c;
+        }
+    }
+    else
+    {
+        if (c > max)
+        {
+            max = c;
+        }
+    }
+    return max;
+}
+int num = MaxNumber(8, 10, 6);
+Console.WriteLine($"max={num}");
