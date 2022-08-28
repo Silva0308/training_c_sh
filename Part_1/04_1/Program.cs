82ce2cdeb74dcb8fbc7e60543d61b4cb9a3a55c5
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-28 21:29:22 +0300

    max number with array

---
+++ Part_1/04_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4. Найти максимальное из трех чисел
+int MaxNumber(int a, int b, int c)
+{
+    int [] num = {a,b,c};
+    int max = num [0];
+    for (int i = 1; i < num.Length; i++)
+    {
+        if (num[i] > max)
+        {
+            max = num [i];
+        }
+    }
+    return max;
+}
+int num = MaxNumber(24, 10, 34);
+Console.WriteLine($"max={num}");
