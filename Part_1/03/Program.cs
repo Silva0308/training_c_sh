9b7475b5e9d7d681776cc701ffb0136a8b036638
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-28 21:08:57 +0300

    day number

---
+++ Part_1/03/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. По заданному номеру дня недели вывести его названиe
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.WriteLine("Enter day number");
+int a =int.Parse(Console.ReadLine());
+
+if (a<1 || a>7)
+{
+    Console.WriteLine("Wrong number");
+}
+else
+{
+    Console.WriteLine(Days [a-1]);
+}
