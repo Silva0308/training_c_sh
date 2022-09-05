69d4436b052e7dfffed20975765c56e8dda21418
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-05 17:24:35 +0300

    even-odd

---
+++ Part_1/06/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Выяснить является ли число чётным
+Console.WriteLine("Please write a number");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine ("It is an even number.");
+}
+else
+{
+    Console.WriteLine ("It is an odd number.");
+}
