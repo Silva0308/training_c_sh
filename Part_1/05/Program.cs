c3bb9db397b242b99a9d299140ff0686a4182ce4
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-05 17:18:41 +0300

    function y=f(a)

---
+++ Part_1/05/Program.cs
@@ -0,0 +1,6 @@
+﻿// 5. Написать программу вычисления значения функции y = f(a)
+
+Console.WriteLine("Please enter some number");
+int a =int.Parse(Console.ReadLine());
+double y = 3*a-5;
+Console.WriteLine ($"Function is y=3*a-5, y={y}");
