3a18eb83e282f549e2c1430c65cee332bc23db1c
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-05 17:40:52 +0300

    row of numbers

---
+++ Part_1/07/Program.cs
@@ -0,0 +1,26 @@
+﻿// 7. Показать числа от -N до N
+
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>0)
+{
+    int a = n*-1;
+    while (a<=n)
+    {
+        Console.Write ($"{a}; ");
+        a++;
+    }
+}
+if (n == 0)
+{
+    Console.WriteLine ("0");
+}
+else
+{
+    int b = n*-1;
+     while (b>=n)
+     {
+        Console.Write ($"{b}; ");
+        b--;
+     }
+}
