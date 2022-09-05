4772e881a8769f1918e80d9dd4586a07e5d05a42
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-05 17:46:17 +0300

    row of even numbers

---
+++ Part_1/08/Program.cs
@@ -0,0 +1,8 @@
+﻿// 8. Показать четные числа от 1 до N
+
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i<=n; i+=2)
+{
+    Console.Write($"{i}, ");
+}
