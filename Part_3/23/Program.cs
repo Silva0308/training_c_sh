eee766f2ad9ad207048b20de4d363944b61f71a9
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:02:31 +0300

    squares

---
+++ Part_3/23/Program.cs
@@ -0,0 +1,11 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+void Squares (int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i*i);
+    }
+}
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+Squares (n);
