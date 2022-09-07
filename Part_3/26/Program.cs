55ec89053b43dfdc6fcf1ca671eb4dc5a38f8dd6
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:21:15 +0300

    multiple

---
+++ Part_3/26/Program.cs
@@ -0,0 +1,15 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+int Multiple(int a, int b)
+{
+    int result = a;
+    for (int i = 2; i<=b; i++)
+    {
+       result*=a;
+    }
+return result;
+}
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please write a second number");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Multiple(n,m));
