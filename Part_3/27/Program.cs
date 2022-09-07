53187a8fc0a41325acbd4e0c4306c9cfe432ba7f
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:27:50 +0300

    number lenght

---
+++ Part_3/27/Program.cs
@@ -0,0 +1,8 @@
+﻿// 27. Определить количество цифр в числе
+void NumberLenght (string n)
+{
+Console.WriteLine(n.Length);
+}
+Console.WriteLine("Please write a number");
+string SomeNumberText = Convert.ToString(Console.ReadLine());
+NumberLenght (SomeNumberText);
