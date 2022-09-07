37b125f74354c98d050694ebd2139f225c75b6ed
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 12:55:48 +0300

    remainder of division

---
+++ Part_1/13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Please write the first number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please write the second number");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n%m == 0)
+{
+    Console.WriteLine ($"{n} is multiples of {m}");
+}
+else
+{
+    Console.WriteLine ($"the remainder of the division is {n%m}");
+}
