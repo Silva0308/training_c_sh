c5a329de9d7e544a13d867c4885ec577cebfbaa8
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:14:17 +0300

    factorial

---
+++ Part_3/25/Program.cs
@@ -0,0 +1,20 @@
+﻿// 25. Найти сумму чисел от 1 до А
+int Factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int factorial = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial += i;
+        }
+        return factorial;
+    }
+}
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(n));
