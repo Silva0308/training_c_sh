2ee2b3602c39384ff6ce58b35295cce34ea724cf
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 13:31:31 +0300

    squares

---
+++ Part_2/17/Program.cs
@@ -0,0 +1,17 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Please write the first number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please write the second number");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n/m == m)
+{
+    Console.WriteLine($"{n} is the square of the {m}");
+}
+else if (m/n == n)
+{
+    Console.WriteLine($"{m} is the square of the {n}");
+}
+else
+{
+    Console.WriteLine("This numbers are not squares");
+}
