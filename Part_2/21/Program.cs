728bee0f130ad8d396d8adfa15fd30f99d033cde
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 17:36:13 +0300

    palindrom

---
+++ Part_2/21/Program.cs
@@ -0,0 +1,23 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Please put 5 digit number");
+int n =Convert.ToInt32(Console.ReadLine());
+if (n/10000 == n%10 && (n/1000)%10 == (n%100)/10) Console.WriteLine("It is a palindrome");
+else Console.WriteLine("It is NOT a palindrome");
+
+//второй способ
+Console.WriteLine("Please put number/word");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+if (IsPalindrome()) Console.WriteLine($"Number/word- {number} - is palindrome");
+else Console.WriteLine($" {number} is not a palindrome");
