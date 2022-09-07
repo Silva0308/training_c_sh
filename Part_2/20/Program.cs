fcaf0e9393cf016fbc3300d22f9f7123ed74813b
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 16:20:27 +0300

    Quarter

---
+++ Part_2/20/Program.cs
@@ -0,0 +1,8 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Please write the quarter number");
+int quarter = Convert.ToInt32(Console.ReadLine());
+if (quarter < 1 || quarter > 4)  Console.WriteLine("It is not a quarter number");
+else if (quarter == 1) Console.WriteLine("x<0, y >0");
+else if (quarter == 2)  Console.WriteLine("x>0, y >0");
+else if (quarter == 3)  Console.WriteLine("x<0, y <0");
+else Console.WriteLine("x>0, y <0");
