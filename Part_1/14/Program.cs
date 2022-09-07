dfc155c1930ba78de46d03e02c57977310a60432
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 13:02:06 +0300

    the third digit

---
+++ Part_1/14/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Please write some number");
+string n = Convert.ToString(Console.ReadLine());
+if (n.Length>=3)
+{
+    Console.WriteLine($"The third digit of this number is {n [2]}");
+}
+else
+{
+    Console.WriteLine("There is no the third digit");
+}
