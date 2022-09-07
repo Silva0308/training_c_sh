5a701989e4b89deb2f153cd5e32df7e20cb39a32
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:42:18 +0300

    sum number

---
+++ Part_3/28/Program.cs
@@ -0,0 +1,13 @@
+﻿// 28. Подсчитать сумму цифр в числе
+int SumNum (int n)
+{
+    int result = 0;
+    string SomeNumberText = Convert.ToString(n);
+    for (int i = 0; i <= SomeNumberText.Length; i++)
+    {
+        result += n%10;
+        n/=10;
+    }
+    return result;
+}
+Console.WriteLine(SumNum(12711));
