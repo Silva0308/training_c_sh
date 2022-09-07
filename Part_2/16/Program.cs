7da953529065c9e4fc7e299be0b10083632fe678
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 13:24:21 +0300

    weekdays

---
+++ Part_2/16/Program.cs
@@ -0,0 +1,15 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Please write a day number!");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 7 || day < 1)
+{
+    Console.WriteLine("Error");
+}
+else if (day ==6 || day == 7)
+{
+    Console.WriteLine("It is a weekend");
+}
+else
+{
+    Console.WriteLine("It is a working day");
+}
