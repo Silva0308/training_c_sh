814252cd5db610409563d306cca0c34c8d897654
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 15:58:27 +0300

    check approval

---
+++ Part_2/18/Program.cs
@@ -0,0 +1,14 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int x = a;
+int y = b;
+
+bool XY = x != a || y == b;
+bool notXY = XY != true;
+bool notXnotY = x != a | y != b;
+System.Console.WriteLine(notXnotY);
+System.Console.WriteLine(notXY);
+
+if (notXY == notXnotY) System.Console.WriteLine("Утвеждение истинно");
+else System.Console.WriteLine("Утверждение ложно");
