22e1372dba3bf6bf22c1d5232d8f05ef64a5811b
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 12:39:08 +0300

    max number

---
+++ Part_1/11/Program.cs
@@ -0,0 +1,11 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int a = new Random().Next(10,100);
+Console.WriteLine(a);
+if (a%10>a/10)
+{
+    Console.WriteLine(a%10);
+}
+else
+{
+    Console.WriteLine(a/10);
+}
