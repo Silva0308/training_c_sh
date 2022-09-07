c0f3b490a5550c20c2ef9ede1ad1c5f30a24f575
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 13:08:11 +0300

    division 7 and 23

---
+++ Part_2/15/Program.cs
@@ -0,0 +1,11 @@
+﻿//15. Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%7 == 0 && n%23== 0 )
+{
+    Console.WriteLine ($"{n} is multiples of 7 and 23");
+}
+else
+{
+    Console.WriteLine ($"{n} is not multiples of 7 and 23");
+}
