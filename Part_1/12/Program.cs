58a3018f5af3fff544e67486e6a77ec7f41f605d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 12:48:49 +0300

    delete second number

---
+++ Part_1/12/Program.cs
@@ -0,0 +1,4 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Please write a three-digit number");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ($"{n/100}{n%10}");
