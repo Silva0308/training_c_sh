9162f289177011d9e080c9466bc395e8c4d8008a
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-07 22:07:03 +0300

    cubes

---
+++ Part_3/24/Program.cs
@@ -0,0 +1,11 @@
+﻿// 24. Найти кубы чисел от 1 до N
+void Cubes(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine (Math.Pow(i,3));
+    }
+}
+Console.WriteLine("Please write a number");
+int n = Convert.ToInt32(Console.ReadLine());
+Cubes (n);
