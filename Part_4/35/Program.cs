9850af6febb60fae62d56613bccb14b7cb97a301
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 17:39:20 +0300

    is number there

---
+++ Part_4/35/Program.cs
@@ -0,0 +1,40 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+bool IsNum (int [] array, int n)
+{
+    bool flag = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == n)
+    {
+        flag = true;
+        Console.WriteLine($"There is number {n} in array.");
+        return flag;
+    }
+}
+    if (flag== false)
+    {
+        Console.WriteLine($"There is no number {n} in array.");
+        return flag;
+    }
+return flag;
+}
+int [] newArray = FillArray (8, -9, 10);
+PrintArray (newArray);
+Console.WriteLine(IsNum (newArray, 5));
