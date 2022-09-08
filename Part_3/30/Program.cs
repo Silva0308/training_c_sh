b476f0023542308256f8aca0342bddf515c2c361
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 10:01:13 +0300

    even cubes

---
+++ Part_3/30/Program.cs
@@ -0,0 +1,12 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+void EvenCubes (int n)
+{
+    for (int i = 1; i <=n; i++)
+    {
+        if ((Math.Pow(i,3))%2 == 0)
+        {
+            Console.WriteLine (Math.Pow(i,3));
+        }
+    }
+}
+EvenCubes (10);
