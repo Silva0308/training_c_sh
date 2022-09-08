87d86061f52b6615c4398447a31906c90b0f0662
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 10:13:54 +0300

    fill array with 0 and 1

---
+++ Part_4/32/Program.cs
@@ -0,0 +1,21 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+}
+
+int [] newArray = FillArray (8, 0, 2);
+PrintArray (newArray);
