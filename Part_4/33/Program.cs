a61ab51d14a0f81198dc3db9f951ad439e93106b
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 10:21:04 +0300

    sum array

---
+++ Part_4/33/Program.cs
@@ -0,0 +1,36 @@
+﻿// 33. Задать массив из 12 элементов, заполненных числами из [-9,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int SumArray (int [] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >0)
+        {
+            result += array[i];
+        }
+    }
+    return result;
+}
+
+int [] newArray = FillArray (12, -9, 10);
+PrintArray (newArray);
+Console.WriteLine ($"Sum of positive elements is {SumArray(newArray)}");
