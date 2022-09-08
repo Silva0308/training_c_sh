6ee9aeb194a7198cb6010c4eaf72d86a6452efb0
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 09:54:36 +0300

    factorial

---
+++ Part_3/29/Program.cs
@@ -0,0 +1,11 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+int Factorial (int n)
+{
+int result = 1;
+for (int i = 1; i <=n; i++)
+{
+    result *=i;
+}
+return result;
+}
+Console.WriteLine(Factorial(5));
