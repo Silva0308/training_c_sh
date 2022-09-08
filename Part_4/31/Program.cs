2e53edc360e262a845de5ad385ac7b96d37277ef
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 10:10:41 +0300

    fill and print array

---
+++ Part_4/31/Program.cs
@@ -0,0 +1,20 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+}
+
+int [] newArray = FillArray (8, 1, 10);
+PrintArray (newArray);
