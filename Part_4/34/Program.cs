4892635fbaed0307922920f8943f4efb50c3d858
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 17:12:18 +0300

    array inversion

---
+++ Part_4/34/Program.cs
@@ -0,0 +1,32 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int [] ArrayInversion (int [] array)
+{
+    for (int i = 0; i <array.Length; i++)
+    {
+        array[i]*=-1;
+    }
+return array;
+}
+
+int [] newArray = FillArray (8, -9, 10);
+PrintArray (newArray);
+int [] invArray = ArrayInversion (newArray);
+PrintArray (invArray);
