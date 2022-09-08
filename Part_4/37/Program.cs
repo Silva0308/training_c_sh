4b42c735d1ebc66ab5569f860a3c9c048a8f778d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-08 17:54:56 +0300

    numbers count

---
+++ Part_4/37/Program.cs
@@ -0,0 +1,35 @@
+﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+void NumCount (int [] array)
+{
+    int count=0;
+    foreach (int item in array)
+    {
+        if (item >= 10 && item <=99)
+        {
+            count ++;
+        }
+    }
+  Console.WriteLine($"Quontity of numbers from [10,99] ia {count}");  
+}
+int [] newArray = FillArray (123, 0, 120);
+PrintArray (newArray);
+NumCount(newArray);
