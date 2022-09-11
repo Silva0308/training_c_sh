df9d64b2e56524cb8e65de2c2bf9e199a84b54da
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-11 11:15:26 +0300

    summ odd numbers

---
+++ Part_4/38/Program.cs
@@ -0,0 +1,31 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int [] FillArray (int n, int min, int max)
+{ 
+    int [] array = new int [n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(min,max);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+ int SumOdd (int [] array)
+ {
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum+= array[i];
+    }
+    return sum;
+ }
+
+ int [] newArray = FillArray (8, -9, 10);
+PrintArray (newArray);
+Console.WriteLine(SumOdd(newArray));
