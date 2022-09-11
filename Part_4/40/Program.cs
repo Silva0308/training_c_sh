2213d265b0f89f96ee69053150c6cf9c8b9d02d3
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-11 11:59:00 +0300

    diff min-max

---
+++ Part_4/40/Program.cs
@@ -0,0 +1,42 @@
+﻿// 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double [] FillArray (int n, double min, double max)
+{ 
+    double [] array = new double [n];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = Math.Round(min + rand.NextDouble() * (max - min), 2);
+    }
+    return array;
+}
+void PrintArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+double DifMinMax (double [] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i])
+            min = array[i];
+        if (max < array[i])
+            max = array[i];
+   }
+    Console.WriteLine("max = {0}", max);
+    Console.WriteLine("min = {0}", min);
+    double result = Math.Round(max-min,2);
+    return result;
+}
+
+
+double [] newArray = FillArray (8, 0, 10);
+PrintArray (newArray);
+double diff = DifMinMax(newArray);
+Console.WriteLine (diff);
