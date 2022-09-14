35d888903d945ab8483040bd9ab648a85713b266
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-14 20:56:37 +0300

    counting positive numbers

---
+++ Part_5/42/Program.cs
@@ -0,0 +1,12 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+Console.WriteLine ("Please, write some numbers");
+var numbers = Console.ReadLine()
+        .Split(new[] {" "},StringSplitOptions.RemoveEmptyEntries)
+        .Select(Int32.Parse);
+int count =0;
+foreach(var n in numbers)
+  if (n>0)
+  {
+    count++;
+  }
+  Console.WriteLine($"You wrote {count} positive numbers");
