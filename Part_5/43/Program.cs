36439bbe34a5a34d7b5c0c3de71c14598bb200dc
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-14 21:38:09 +0300

    binar numbers

---
+++ Part_5/43/Program.cs
@@ -0,0 +1,12 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+void DiNum (int num)
+{ 
+	string result = String.Empty;
+while (num != 0)
+{ 
+    result=num%2+result;
+    num/=2;
+} 
+Console.WriteLine(result);
+}
+DiNum(75);
