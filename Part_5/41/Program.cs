0a9a41454facde56682b586007e0587ea813604c
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-14 20:47:12 +0300

    triangle

---
+++ Part_5/41/Program.cs
@@ -0,0 +1,8 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+void Triagle(int a, int b, int c)
+{
+    if (a+b>c && a+c>b&& b+c>a) Console.WriteLine ("It is a triangle");
+    
+    else Console.WriteLine ("It is NOT a triangle");
+}
+Triagle(10,4,5);
