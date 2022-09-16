46b5e1dea4fff96e8e3a398f0da0c3a254b9b2e7
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-16 09:30:55 +0300

    fibinacci

---
+++ Part_5/45/Program.cs
@@ -0,0 +1,25 @@
+﻿// 45. Показать числа Фибоначчи
+int[] FibonacciNum(int n)
+{
+    int[] array = new int[n];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+void Print(int[] arr) 
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] fib = FibonacciNum (8);
+Print (fib);
