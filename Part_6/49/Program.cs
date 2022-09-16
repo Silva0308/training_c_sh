2bedb3ba24f08a5903c180f0e26b59bd0bea22ed
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-16 14:45:53 +0300

    double numbers array

---
+++ Part_6/49/Program.cs
@@ -0,0 +1,32 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+void Print(double[,] array)
+{
+    int row_size = array.GetLength(0);
+    int column_size = array.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write("{0,6:F2}",array[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double [,] MassNum(int row, int column, double from , double  to)
+{
+    double[,] array = new double [row, column];
+    Random rand = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = Math.Round(from + rand.NextDouble() * (to - from), 2);
+        } 
+    }
+    return array;
+}
+
+double [,] matrix = MassNum(4,4,0,10);
+Print(matrix);
